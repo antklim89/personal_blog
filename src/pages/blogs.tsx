8ca671fad89584d65215e1e4e918e9a6788548f7
Anton <antklim89@mail.ru>
2022-03-26 16:25:56 +0300

    added PostsList

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,17 +1,37 @@
-import { PageProps } from 'gatsby';
+import { Container } from '@chakra-ui/react';
+import { graphql, PageProps } from 'gatsby';
 import { FC } from 'react';
 
 import { Seo } from '~/components/Seo';
+import PostsList from '~/layouts/PostsList';
+import { IPost } from '~/types';
 
 
-const BlogsPage: FC<PageProps> = () => {
+const BlogsPage: FC<PageProps<{ allMarkdownRemark: { nodes: IPost[] } }>> = ({ data }) => {
     return (
         <>
             <Seo title="Blogs" />
-            <div>BLOGS</div>
+            <Container maxW="container.lg" my={10}>
+                <PostsList posts={data.allMarkdownRemark.nodes} />
+            </Container>
         </>
     );
 };
 
 export default BlogsPage;
 
+export const query = graphql`
+  query PostsList{
+    allMarkdownRemark(
+        filter: {
+            fileAbsolutePath: { regex: "/\\/posts\\/.*\\.md/i" },
+            frontmatter: { hidden: { eq: false } }
+        }
+    ) {
+      nodes {
+        ...PostFragment
+        body: excerpt(format: HTML, pruneLength: 300)
+      }
+    }
+  }
+`;
