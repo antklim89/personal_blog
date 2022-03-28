24f3f99881aa5c03221f869afbf25100b116a404
Anton <antklim89@mail.ru>
2022-03-28 03:17:46 +0300

    updated contacts form

--- src/layouts/Contacts/Contacts.tsx
+++ src/layouts/Contacts/Contacts.tsx
@@ -1,33 +1,45 @@
-import { Box, Heading, Input, Textarea } from '@chakra-ui/react';
+import { Box, Button, Heading, Input, Textarea } from '@chakra-ui/react';
 import { FC } from 'react';
 
-import { ContactsProps } from './Contacts.types';
 
-
-const Contacts: FC<ContactsProps> = () => {
+const Contacts: FC = () => {
 
     return (
         <Box as="section">
             <Heading as="h1" my={4}>
                 Contact me
             </Heading>
-            <Box as="form">
+            <Box
+                as="form"
+                data-netlify="true" 
+                display="flex"
+                flexDir="column"
+            >
                 <Input
                     mb={4}
+                    name="name"
                     placeholder="Your name"
                     type="text"
                 />
                 <Input
                     mb={4}
+                    name="email"
                     placeholder="Your email"
                     type="email"
                 />
                 <Textarea
                     mb={4}
+                    name="message"
                     placeholder="Your message"
                     resize="none"
                     rows={5}
                 />
+                <Button
+                    colorScheme="primary"
+                    type="submit"
+                >
+                    Send
+                </Button>
             </Box>
         </Box>
     );
--- src/layouts/Contacts/Contacts.types.ts
+++
@@ -1,5 +0,0 @@
-
-
-export interface ContactsProps {
-     // null
-}
