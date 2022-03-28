2156bd05c26523f5ceb211319b893db52be56ef5
Anton <antklim89@mail.ru>
2022-03-28 03:29:32 +0300

    updated Header

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,6 +1,5 @@
-import { Stack, Box, Container, Text, Spacer } from '@chakra-ui/react';
+import { Stack, Box, Container } from '@chakra-ui/react';
 import { useTheme } from '@emotion/react';
-import { Link } from 'gatsby';
 import { FC } from 'react';
 
 import HeaderDrawer from './HeaderDrawer';
@@ -10,25 +9,20 @@ const Header: FC = () => {
     const { custom: { headerHeight } } = useTheme() as { custom: {headerHeight: number } };
     return (
         <>
-            <Box as="header" flexShrink={0} sx={{ bg: 'linear-gradient(black, transparent)', pos: 'fixed', width: '100%', zIndex: 100 }}>
+            <Box
+                as="header"
+                background="linear-gradient(black, transparent)"
+                flexShrink={0}
+                position="fixed"
+                width="100%"
+                zIndex={100}
+            >
                 <Container maxW='container.lg'>
                     <Stack
                         alignItems="center"
                         direction="row"
-                        height={16}
-                        spacing='24px'
+                        justifyContent="flex-end"
                     >
-                        <Text
-                            alignItems="center"
-                            as={Link}
-                            color="primary.textLight"
-                            display="flex"
-                            fontSize="x-large"
-                            fontWeight="bold" to="/"
-                        >
-                            <Box ml={[1, 2, 6]}>PERSONAL&nbsp;BLOG</Box>
-                        </Text>
-                        <Spacer />
                         <HeaderDrawer />
                     </Stack>
                 </Container>
