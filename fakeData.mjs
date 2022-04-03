c5135c4fc4b768973456df106dfaf336ffb6d026
Anton <antklim89@mail.ru>
2022-04-03 08:50:58 +0300

    added fake data

--- fakeData.mjs
+++ fakeData.mjs
@@ -1,30 +1,38 @@
 import fs from 'fs';
 import path from 'path';
 
+import { faker } from '@faker-js/faker';
+import _ from 'lodash';
+
+
+const POSTS_QTY = 10;
 
 const postsPath = path.resolve('src/content/posts');
 
-// if (!fs.existsSync(postPath)) {
-if (true) {
-    fs.mkdirSync(postsPath, { recursive: true });
+
+fs.mkdirSync(postsPath, { recursive: true });
     
-    for (let index = 0; index < 5; index++) {
+for (let index = 0; index < POSTS_QTY; index++) {
 
-        const title = `Semper eget duis ${Math.random()}`;
-        const fileName = `${title}.md`;
+    const title = _.upperFirst(faker.lorem.words(3));
+    const fileName = `${_.kebabCase(title)}.md`;
 
 
-        const post = `
----
+    const post = `---
 hidden: false
 title: ${title}
-createdAt: 2022-03-24T16:34:17.340Z
-imagePreview: images/yana-marudova-q4vustngxm8-unsplash.jpg
+createdAt: ${faker.date.between('2020-01-01T00:00:00.000Z', new Date().toISOString()).toISOString()}
+imagePreview: images/image${_.random(1, 5)}.jpg
 ---
-Tristique senectus at nam at
+${faker.lorem.paragraph(_.random(20, 100))}
+
+${faker.lorem.paragraph(_.random(20, 100))}
+
+${faker.lorem.paragraph(_.random(20, 100))}
+
+${faker.lorem.paragraph(_.random(20, 100))}
         `;
 
-        fs.writeFileSync(path.resolve(postsPath, fileName), post);
-    }
-
+    fs.writeFileSync(path.resolve(postsPath, fileName), post);
 }
+
