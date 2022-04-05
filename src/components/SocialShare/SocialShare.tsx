3ec2b7ed406fc8f43512a89437f94085e833b7a7
Anton <antklim89@mail.ru>
2022-04-05 21:45:12 +0300

    updated SocialShare

--- src/components/SocialShare/SocialShare.tsx
+++ src/components/SocialShare/SocialShare.tsx
@@ -21,32 +21,31 @@ const SocialShare: FC<SocialShareProps> = ({ title, image }) => {
         if (location.href && !isLoad) setIsLoad(true);
     });
 
-    
     if (!isLoad) return null;
     return (
         <HStack sx={{ svg: { width: 12 } }}>
             <VKShareButton 
                 image={`${location.origin}${image}`} 
                 title={title} 
-                url="https://personal-blog-dev.netlify.app/posts/ffb85fe6-a6ff-54c6-a5e8-bdca79c6630d" 
+                url={location.href} 
             >
                 <VKIcon />
             </VKShareButton>
             <EmailShareButton 
                 subject={title}
-                url="https://personal-blog-dev.netlify.app/posts/ffb85fe6-a6ff-54c6-a5e8-bdca79c6630d"
+                url={location.href}
             >
                 <EmailIcon />
             </EmailShareButton>
             <FacebookShareButton 
                 title={title}
-                url="https://personal-blog-dev.netlify.app/posts/ffb85fe6-a6ff-54c6-a5e8-bdca79c6630d"
+                url={location.href}
             >
                 <FacebookIcon />
             </FacebookShareButton>
             <RedditShareButton 
                 title={title}
-                url="https://personal-blog-dev.netlify.app/posts/ffb85fe6-a6ff-54c6-a5e8-bdca79c6630d"
+                url={location.href}
             >
                 <RedditIcon />
             </RedditShareButton>
