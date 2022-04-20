013ff45f4839fb8772abcef28bdfbcf942125911
Anton <antklim89@mail.ru>
2022-04-20 20:15:45 +0300

    updated Post component

--- src/layouts/Post/Post.tsx
+++ src/layouts/Post/Post.tsx
@@ -1,6 +1,6 @@
 import { Box, Heading, Text } from '@chakra-ui/react';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { PostProps } from './types';
 
@@ -8,15 +8,27 @@ import SocialShare from '~/components/SocialShare';
 
 
 const Post: FC<PostProps> = ({ body, title, imagePreview, createdAt }) => {
+    useEffect(() => {
+        const bodyEl = document.querySelector('body');
+        if (bodyEl) bodyEl.scrollTop = 0;
+    }, []);
+
     return (
         <Box as="article" mb={10}>
-            <Box mb={4}>
+            <Box
+                left={0}
+                mb={4}
+                position="absolute"
+                right={0}
+                sx={{ '& *': { width: '100%', height: 270 } }}
+                top={0}
+            >
                 <GatsbyImage
                     alt="post prewiew image"
                     image={imagePreview.gatsbyImageData}
                 />
             </Box>
-            <Heading as="h1">
+            <Heading as="h1" marginTop={270}>
                 {title}
             </Heading>
             <SocialShare
