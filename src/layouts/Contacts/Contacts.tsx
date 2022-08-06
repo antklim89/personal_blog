963a774c29b11b90381050b4f1943c7833fa711f
Anton <antklim89@mail.ru>
2022-08-06 21:40:43 +0300

    update contacts form

--- src/layouts/Contacts/Contacts.tsx
+++ src/layouts/Contacts/Contacts.tsx
@@ -1,54 +1,93 @@
-import { Box, Button, Input, Text, Textarea } from '@chakra-ui/react';
-import { FC } from 'react';
+import {
+    Alert, AlertIcon, Box, Button, Input, Text, Textarea,
+} from '@chakra-ui/react';
+import { FC, FormEventHandler, useState } from 'react';
 
 
 const Contacts: FC = () => {
+    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState<'success' | 'error' | null>(null);
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const body = new FormData(e.currentTarget);
+        const response = await fetch('/', { method: 'POST', body });
+
+        if (response.ok) {
+            setStatus('success');
+            (e?.target as HTMLFormElement)?.reset?.();
+        }
+        else setStatus('error');
+
+        setLoading(false);
+    };
 
     return (
         <Box as="section">
             <Text fontSize='5xl' textAlign="center">
                 Have a question?<br />Contact me.
             </Text>
-            <Box
-                action='/contacts/success'
-                as="form"
+
+            {status && (
+                <Alert my={4} status={status}>
+                    <AlertIcon />
+                    {status === 'success' && 'The message has been sent successfully'}
+                    {status === 'error' && 'Unexpected error. Try again later.'}
+                </Alert>
+            )}
+
+            <form
                 data-netlify="true"
-                display="flex"
-                flexDir="column"
-                method="post"
                 name="contact"
                 netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
             >
-                <Box as="label" display="none">
-                    Don’t fill this out if you’re human: <input name="bot-field" />
-                </Box>
-                <Input
-                    mb={4}
-                    name="name"
-                    placeholder="Your name"
-                    type="text"
-                />
-                <Input
-                    mb={4}
-                    name="theme"
-                    placeholder="Your message theme"
-                    type="text"
-                />
-                <Textarea
-                    mb={4}
-                    name="message"
-                    placeholder="Your message"
-                    resize="none"
-                    rows={5}
-                />
-                <input name="form-name" type="hidden" value="contact" />
-                <Button
-                    colorScheme="primary"
-                    type="submit"
+                <Box
+                    display="flex"
+                    flexDir="column"
                 >
-                    Send
-                </Button>
-            </Box>
+                    <Box as="label" display="none">
+                        Don’t fill this out if you’re human: <input name="bot-field" />
+                    </Box>
+                    <Input
+                        maxLength={100}
+                        mb={4}
+                        minLength={3}
+                        name="name"
+                        placeholder="Your name"
+                        type="text"
+                    />
+                    <Input
+                        maxLength={200}
+                        mb={4}
+                        minLength={3}
+                        name="subject"
+                        placeholder="Your message subject"
+                        type="subject"
+                    />
+                    <Textarea
+                        maxLength={2000}
+                        mb={4}
+                        minLength={10}
+                        name="message"
+                        placeholder="Your message"
+                        resize="none"
+                        rows={5}
+                    />
+                    <input name="form-name" type="hidden" value="contact" />
+                    <Button
+                        colorScheme="primary"
+                        disabled={loading}
+                        isLoading={loading}
+                        loadingText='Sending'
+                        type="submit"
+                    >
+                        Send
+                    </Button>
+                </Box>
+            </form>
         </Box>
     );
 };
