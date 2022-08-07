fe4fd043db759a025a681114845cc7e5363d0c58
Anton <antklim89@mail.ru>
2022-08-07 10:57:07 +0300

    add scrollTop to PostsList

--- src/layouts/PostsList/PostsList.tsx
+++ src/layouts/PostsList/PostsList.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import PostsItem from './PostsItem';
 import { PostsListProps } from './types';
 
 
 const PostsList: FC<PostsListProps> = ({ posts }) => {
+    useEffect(() => { document.body.scrollTop = 0; }, []);
+
     return (
         <Container>
             {posts.map((post) => (
