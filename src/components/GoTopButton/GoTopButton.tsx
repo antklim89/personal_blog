9fa85cf26ae05b115558b6ada66eb44e1bd904e4
Anton <antklim89@mail.ru>
2023-02-20 00:40:23 +0300

    create GoTopButton component

---
+++ src/components/GoTopButton/GoTopButton.tsx
@@ -0,0 +1,23 @@
+import { ChevronUpIcon } from '@chakra-ui/icons';
+import { Button, Flex } from '@chakra-ui/react';
+import { FC, useCallback, useEffect, useState } from 'react';
+
+
+export const GoTopButton: FC = () => {
+    const goTop = useCallback(() => { document.body.scrollTop = 0; }, []);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        const { scrollHeight, clientHeight } = document.body;
+        setShow(scrollHeight > clientHeight);
+    }, []);
+    
+    if (!show) return null;
+    return (
+        <Flex alignSelf="center" mt={50}>
+            <Button colorScheme="primary" px={8} onClick={goTop}>
+                <ChevronUpIcon />
+            </Button>
+        </Flex>
+    );
+};
