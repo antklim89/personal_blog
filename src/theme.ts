e84b429a3f5cb539ef6a175e66423543f324094c
Anton <antklim89@mail.ru>
2023-02-27 16:22:59 +0300

    update Button theme styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,21 +1,21 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeOverride  } from '@chakra-ui/react';
 
 
-export default extendTheme({
+const config: ThemeOverride = {
     colors: {
         primary: {
-            50: 'hsl(36, 42%, 90%)',
-            100: 'hsl(36, 42%, 85%)',
-            200: 'hsl(36, 42%, 80%)',
-            300: 'hsl(36, 42%, 70%)',
-            400: 'hsl(36, 42%, 60%)',
-            500: 'hsl(36, 42%, 55%)',
-            600: 'hsl(36, 42%, 51%)',
-            700: 'hsl(36, 42%, 40%)',
-            800: 'hsl(36, 42%, 30%)',
-            900: 'hsl(36, 42%, 20%)',
-            text: 'black',
-            textLight: 'white',
+            50: 'hsl(60, 10%, 24%)',
+            100: 'hsl(60, 10%, 28%)',
+            200: 'hsl(60, 10%, 32%)',
+            300: 'hsl(60, 10%, 36%)',
+            400: 'hsl(60, 10%, 40%)',
+            500: 'hsl(60, 10%, 44%)',
+            600: 'hsl(60, 10%, 50%)',
+            700: 'hsl(60, 10%, 54%)',
+            800: 'hsl(60, 10%, 58%)',
+            900: 'hsl(60, 10%, 62%)',
+            text: 'hsl(60, 10%, 5%)',
+            textLight: 'hsl(60, 10%, 90%)',
         },
     },
     styles: {
@@ -30,7 +30,21 @@ export default extendTheme({
         },
     },
     components: {
-        Button: { baseStyle: { cursor: 'pointer' } },
+        Button: {
+            baseStyle: {
+                cursor: 'pointer',
+                borderTop: 'none',
+                borderRadius: 0,
+                display: 'flex',
+                alignItems: 'center',
+                width: 'min-content',
+                lineHeight:0,
+            },
+            variants: { outline: { _hover:{ color: ({ colors }) => colors.primary['textLight'] } } },
+        },
         Container: { baseStyle: { maxWidth: 'container.lg' } },
     },
-});
+};
+
+
+export default extendTheme(config);
