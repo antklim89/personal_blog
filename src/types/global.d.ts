ffc5c591f65854c39961e727806d1934862dd3be
Anton <antklim89@mail.ru>
2023-03-03 12:28:01 +0300

    create DeepRequired type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,4 @@
-
+/* eslint-disable @typescript-eslint/ban-types */
 
 declare module '*.webp' {
     export default string;
@@ -7,4 +7,9 @@ declare module '*.webp' {
 
 declare type DeepPartial<T> = Partial<{
     [K in keyof T]: DeepPartial<T[K]>
+}>
+
+
+declare type DeepRequired<T> = NonNullable<{
+    [K in keyof T]-?: DeepRequired<T[K] & {}>
 }>
