8bf352599a869936b77a67202bb06ecb9783d00b
Anton <=>
2024-06-26 15:54:11 +0300

    refactor: update PostsItem

--- src/layouts/PostsList/PostsItem.tsx
+++ src/layouts/PostsList/PostsItem.tsx
@@ -1,37 +1,70 @@
-import { Box, Button, Divider, Heading } from '@chakra-ui/react';
-import { Link } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
+import { Box, Button, useColorModeValue } from '@chakra-ui/react';
 import { FC } from 'react';
-
+import { Flex, Image, Link } from '@chakra-ui/react';
+import { type IPostPreview } from '~/types';
+import { Link as GatsbyLink } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import FromNow from '~/components/FromNow';
-import { IPostPreview } from '~/types';
 
 
 const PostsItem: FC<IPostPreview> = ({ id, title, imagePreview, createdAt, bodyPreview }) => {
+    const bgColor = useColorModeValue('gray.100', 'gray.700');
+
     return (
-        <Box as="article" sx={{ 'img': { bgColor: 'white' } }}>
-            <Box mb={4}>
-                <GatsbyImage
-                    alt="post prewiew image"
-                    image={imagePreview.gatsbyImageData}
-                />
-            </Box>
-            <Heading as="h3">
-                {title}
-            </Heading>
-            <FromNow date={createdAt} mb={4} />
-            <Box dangerouslySetInnerHTML={{ __html: bodyPreview }} my={4} />
-            <Button
-                as={Link}
-                colorScheme="primary"
-                marginLeft="auto"
-                to={`/posts/${id}`}
-                variant="outline"
+        <Flex
+            w="full"
+            alignItems="center"
+            justifyContent="center"
+            bg={bgColor}
+            m={8}
+        >
+            <Box
+                mx="auto"
+                rounded="lg"
+                shadow="md"
             >
-                Read more...
-            </Button>
-            <Divider mb={10} mt={4} />
-        </Box>
+                <Image
+                    as={GatsbyImage}
+                    roundedTop="lg"
+                    w="full"
+                    h={64}
+                    fit="cover"
+                    image={imagePreview.gatsbyImageData}
+                    alt="Article photo"
+                />
+                <Box p={8}>
+                    <Box>
+                        <h2>
+                            <Link
+                                as={GatsbyLink}
+                                to={`/posts/${id}`}
+                                display="block"
+                                fontWeight="bold"
+                                fontSize="4xl"
+                            >
+                                {title}
+                            </Link>
+                        </h2>
+            
+                        <FromNow date={createdAt} mb={4} />
+
+                        <Box dangerouslySetInnerHTML={{ __html: bodyPreview }} my={4} />
+
+                        <Flex>
+                            <Button
+                                as={GatsbyLink}
+                                colorScheme="primary"
+                                marginLeft="auto"
+                                to={`/posts/${id}`}
+                                variant="outline"
+                            >
+                                Read more...
+                            </Button>
+                        </Flex>
+                    </Box>
+                </Box>
+            </Box>
+      </Flex>
     );
 };
 
