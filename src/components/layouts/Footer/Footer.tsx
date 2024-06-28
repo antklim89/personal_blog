48ec7a7168346056cf08ea014703edf2ed818cec
Anton <=>
2024-06-28 11:54:35 +0300

    update footer styles

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, HStack } from '@chakra-ui/react';
+import { Box, Container, Stack, Text } from '@chakra-ui/react';
 import { FC } from 'react';
 
 import { Social } from '~/components';
@@ -6,16 +6,24 @@ import { Social } from '~/components';
 
 const Footer: FC = () => {
     return (
-        <Box as="footer" bgColor="black" color="white">
-            <Container>
-                <HStack justifyContent="space-between">
-                    <span>&copy; {new Date().getFullYear()} Personal Blog</span>
-                    <Social />
-                </HStack>
+        <Box
+            bg="gray.700"
+            color="gray.50"
+        >
+            <Container
+                align={{ base: 'center', md: 'center' }}
+                as={Stack}
+                direction={{ base: 'column', md: 'row' }}
+                justify={{ base: 'center', md: 'space-between' }}
+                maxW="6xl"
+                py={4}
+                spacing={4}
+            >
+                <Text>© 2024 Personal Blog. All rights reserved</Text>
+                <Social fontSize="sm" />
             </Container>
         </Box>
     );
 };
 
 export default Footer;
-
