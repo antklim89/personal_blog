49fcafe3b7988bccf4361ab7ac5ee9ef2ed6feee
Anton <antklim89@mail.ru>
2025-02-23 19:55:27 +0300

    update blog schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,8 +8,7 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    heroImage: z.string(),
   }),
 });
 
