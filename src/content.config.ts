ff0d81809d918932bdde9572c9ce0c8d18ec1392
Anton <antklim89@mail.ru>
2025-03-07 21:02:51 +0300

    feat: update content
    add about and hero collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,14 @@
+import { randomUUID } from 'node:crypto';
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 
 const blog = defineCollection({
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  loader: glob({
+    base: 'src/content/blog',
+    pattern: '**/*.{md,mdx}',
+    generateId: () => randomUUID(),
+  }),
   schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
@@ -12,4 +17,23 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const hero = defineCollection({
+  loader: glob({
+    base: 'src/content/hero',
+    pattern: 'index.md',
+    generateId: () => 'hero',
+  }),
+  schema: ({ image }) => z.object({
+    image: image(),
+  }),
+});
+
+const about = defineCollection({
+  loader: glob({
+    base: 'src/content/about',
+    pattern: 'index.md',
+    generateId: () => 'about',
+  }),
+});
+
+export const collections = { blog, hero, about };
