ed72b8501c8134e558d02106796c5f72b1dd1590
Anton <antklim89@mail.ru>
2025-03-22 14:28:43 +0300

    improve styles

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -14,30 +14,31 @@ import {
 } from 'react-share';
 
 
+const ICON_SIZE = 42;
+
 export default function Share({ title, text }: { title: string; text: string }) {
   const url = location.href;
+
   return (
     <section>
-      <div>
-        <EmailShareButton subject={title} body={text} url={url} htmlTitle={title}>
-          <EmailIcon />
-        </EmailShareButton>
-        <WhatsappShareButton url={url} title={title} htmlTitle={title}>
-          <WhatsappIcon />
-        </WhatsappShareButton>
-        <RedditShareButton url={url} title={title} htmlTitle={title}>
-          <RedditIcon />
-        </RedditShareButton>
-        <TelegramShareButton url={url} title={title} htmlTitle={title}>
-          <TelegramIcon />
-        </TelegramShareButton>
-        <ThreadsShareButton url={url} title={title} htmlTitle={title}>
-          <BlueskyIcon />
-        </ThreadsShareButton>
-        <BlueskyShareButton url={url} title={title} htmlTitle={title}>
-          <ThreadsIcon />
-        </BlueskyShareButton>
-      </div>
+      <EmailShareButton subject={title} body={text} url={url} htmlTitle={title}>
+        <EmailIcon size={ICON_SIZE} />
+      </EmailShareButton>
+      <WhatsappShareButton url={url} title={title} htmlTitle={title}>
+        <WhatsappIcon size={ICON_SIZE} />
+      </WhatsappShareButton>
+      <RedditShareButton url={url} title={title} htmlTitle={title}>
+        <RedditIcon size={ICON_SIZE} />
+      </RedditShareButton>
+      <TelegramShareButton url={url} title={title} htmlTitle={title}>
+        <TelegramIcon size={ICON_SIZE} />
+      </TelegramShareButton>
+      <ThreadsShareButton url={url} title={title} htmlTitle={title}>
+        <ThreadsIcon size={ICON_SIZE} />
+      </ThreadsShareButton>
+      <BlueskyShareButton url={url} title={title} htmlTitle={title}>
+        <BlueskyIcon size={ICON_SIZE} />
+      </BlueskyShareButton>
     </section>
   );
 }
